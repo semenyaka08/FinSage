97db41815ae6d0fcf187c39e0575bbf9b975f724
Semenyaka Sergey <Serh.semka@gmail.com>
2024-08-13 10:13:31 +0300

    Setup models and initial database creation using CodeFirst

---
+++ Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Core.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    { }
+
+    public DbSet<Stock> Stocks { get; set; }
+
+    public DbSet<Comment> Comments { get; set; }
+}
--- Web/Program.cs
+++ Web/Program.cs
@@ -1,3 +1,6 @@
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
